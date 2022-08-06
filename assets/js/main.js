f4f1aedccb6c62d9822ab2bf4f627607a22ae696
jimenes30 <alfredojimenes99@gmail.com>
2022-08-05 23:42:03 -0300

    debug botao e melhorias

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,26 @@
-const evento = () => {
-  if (window.ethereum) {
-    document.querySelector('.botao').addEventListener('click', async () => {
-      try {
-        const accounts = await ethereum.request({method:'eth_requestAccounts'});
-        account = accounts[0];
-        document.querySelector('span').innerHTML = account;
-      } catch(e) {}
-  
-    })
-    
-  } else {
-    alert('Você não tem uma carteira crypto??')
-  }
-}
+const connectButton = document.querySelector('.connect-button');
+
+const userWalletAddress = document.querySelector('.user-wallet')
+
+const userBNB = document.querySelector('.user-bnb')
+const userChainId = document.querySelector('.user-chainid')
+const userMainnetEthereum = document.querySelector('.user-mainnet-ethereum')
+
+const fox = document.getElementById('logo-container')
+
+connectButton.addEventListener('click',  async function connectWallet() {
+  try {
+    if (typeof window.ethereum !== 'undefined') {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      const account = accounts[0];
+      userWalletAddress.innerHTML = ethereum.selectedAddress
+      userChainId.innerHTML = ethereum.networkVersion
+      userMainnetEthereum.innerHTML = ethereum.isConnected()
+      fox.classList.remove('hidden')
+    }
+    else {
+      alert('Metamask is not installed')
+    }
+  } catch (e) {}
+});
+
