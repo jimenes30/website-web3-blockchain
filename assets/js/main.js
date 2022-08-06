5a464bb55114ed6742114bf1bbd78341fa96c121
jimenes30 <alfredojimenes99@gmail.com>
2022-08-06 00:13:06 -0300

    added fox

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ const userMainnetEthereum = document.querySelector('.user-mainnet-ethereum')
 const fox = document.getElementById('logo-container')
 
 connectButton.addEventListener('click',  async function connectWallet() {
+  fox.classList.remove('hidden')
   try {
     if (typeof window.ethereum !== 'undefined') {
       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
@@ -16,7 +17,7 @@ connectButton.addEventListener('click',  async function connectWallet() {
       userWalletAddress.innerHTML = ethereum.selectedAddress
       userChainId.innerHTML = ethereum.networkVersion
       userMainnetEthereum.innerHTML = ethereum.isConnected()
-      fox.classList.remove('hidden')
+      // fox.classList.remove('hidden')
     }
     else {
       alert('Metamask is not installed')
