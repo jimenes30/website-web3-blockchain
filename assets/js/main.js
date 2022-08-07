e717ed12462c69a6cc6792698ce1d864dc9dbe14
jimenes30 <alfredojimenes99@gmail.com>
2022-08-07 02:03:01 -0300

    2 buttons added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const connectButton = document.querySelector('.connect-button');
+const connectButton = document.querySelectorAll('.connect-button');
 
 const userWalletAddress = document.querySelector('.user-wallet')
 
@@ -8,19 +8,22 @@ const userMainnetEthereum = document.querySelector('.user-mainnet-ethereum')
 
 const fox = document.getElementById('logo-container')
 
-connectButton.addEventListener('click',  async function connectWallet() {
-  fox.classList.remove('hidden')
-  try {
-    if (typeof window.ethereum !== 'undefined') {
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-      const account = accounts[0];
-      userWalletAddress.innerHTML = ethereum.selectedAddress
-      userChainId.innerHTML = ethereum.networkVersion
-      userMainnetEthereum.innerHTML = ethereum.isConnected()
-    }
-    else {
-      alert('Metamask is not installed')
-    }
-  } catch (e) {}
-});
+for (var i = 0; i <= 1; i++) {
+  connectButton[i].addEventListener('click',  async function connectWallet() {
+    fox.classList.remove('hidden')
+    try {
+      if (typeof window.ethereum !== 'undefined') {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        userWalletAddress.innerHTML = ethereum.selectedAddress
+        userChainId.innerHTML = ethereum.networkVersion
+        userMainnetEthereum.innerHTML = ethereum.isConnected()
+      }
+      else {
+        alert('Metamask is not installed')
+      }
+    } catch (e) {}
+  });
+}
+
 
